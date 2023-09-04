2f5fa451cee2adfe7585d9effbc5c38dd9dc4528
Elie-MK <elie.mk25@gmail.com>
2023-09-04 14:14:21 +0100

    second commit

---
+++ ios/WelcomeIos.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const WelcomeIos = () => {
+  return (
+    <View>
+      <Text style={{color:"red"}}>WelcomeIos</Text>
+    </View>
+  )
+}
+
+export default WelcomeIos
